ce7f0b543acca0ac6b5780563d4d14bb908561de
charliesouthview <charlesl27857@isd273.org>
2021-12-04 03:16:35 -0600

    pushing prob

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
