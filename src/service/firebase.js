bc8b97f68d6776797e686ff2ad60effdf2ea9ce9
charliesouthview <94203256+charliesouthview@users.noreply.github.com>
2021-12-04 03:36:29 -0600

    Update firebase.js

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/firestore';
 
 try {
   firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: '1:1040930990355:web:faedd6c4860f814db3d2e6',
+  apiKey: "AIzaSyAv96ARtqqfsW083HWi2kW4g-WsUoO314k",
+  authDomain: "svchat-d9cf8.firebaseapp.com",
+  projectId: "svchat-d9cf8",
+  storageBucket: "svchat-d9cf8.appspot.com",
+  messagingSenderId: "1040930990355",
+  appId: "1:1040930990355:web:faedd6c4860f814db3d2e6"
   });
 } catch (error) {
   if (!/already exists/u.test(error.message)) {
